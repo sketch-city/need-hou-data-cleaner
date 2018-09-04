51b937e223d6bae1a9e6553444597b180b88e102
Niha Pereira <niha.pereira@gmail.com>
2018-09-03 19:23:18 -0500

    added postbuild npm thing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 var m = require("mithril")
 
+
+
 var AgencyForm= require("./views/AgencyForm")
 var ProgramForm = require("./views/ProgramForm")
 var Layout = require("./views/Layout")
 var Review = require("./views/Review")
 var NewAgency = require("./views/NewAgency")
 var NewReview = require("./views/NewReview")
-
-
-server.listen(config.port, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
-
-
 
 m.route(document.body, "/agencyform", {
     "/agencyform" : {
