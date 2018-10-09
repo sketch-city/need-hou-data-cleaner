b10137a6abd53526eb9d66b2c6f11fde95d5c852
Niha Pereira <niha.pereira@gmail.com>
2018-10-09 12:54:33 -0500

    linked to needhou website

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -10,7 +10,11 @@ module.exports = {
             	m("ul",
                     m("li[id=title]", "Organization & Service Update Tool"),
         			m("li[id=logout]",
-        				m("a", "Logout"))), vnode.children,
+        				m("a", "Logout")),
+                    m("li[id=needhou]",
+                        m("a", {href: "https://needhouwebsite.herokuapp.com/"}, "Houston Social Services Database"))
+
+                    ), vnode.children,
             m("div[id=myProgress]",
                 m("div[id=myBar]"))
             ))
