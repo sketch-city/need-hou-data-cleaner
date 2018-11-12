4e66d8605fa6be5beabb4e361c1bc0721cddbf2b
Niha Pereira <niha.pereira@gmail.com>
2018-11-12 12:26:54 -0600

    added zipcode options

--- src/views/ReviewFields.js
+++ src/views/ReviewFields.js
@@ -42,18 +42,6 @@ view: function(vnode) {
 					m("p", m("strong", "Waitlist wait: "), m("pre.program[id=waitlist_wait]")),
 					m("p", m("strong", "Client next steps "), m("pre.program[id=next_steps]")),
 					m("p", m("strong", "Program languages: "), m("pre.program[id=language_arr]")),
-					m("p", m("strong", "Current accepted ID's"), m("pre.program[id=id_accepted_current]")),
-					m("p", m("strong", "Expired accepted ID's"), m("pre.program[id=id_accepted_expired]")),
-					m("p", m("strong", "Proof of address"), m("pre.program[id=proof_address]")),
-					m("p", m("strong", "Required document links: "), m("pre.program[id=documents_required]")),
-					m("p", m("strong", "Document languages: "), m("pre.program[id=document_languages]")),
-					m("p", m("strong", "Form assistance offered: "), m("pre.program[id=document_assistance]")),
-					m("p", m("strong", "Service cost: "), m("pre.program[id=service_cost]")),
-					m("p", m("strong", "Payment options: "),m("pre.program[id=fee_structure]")),
-					m("p", m("strong", "Appointment required?: "), m("pre.program[id=appointment_required]")),
-					m("p", m("strong", "Appointment notes: "), m("pre.program[id=appointment_required_notes]")),
-
-					// //eligibility fields
 					m("p", m("strong", "Eligible zipcodes: "), m("pre.program[id=zipcode_eligibility]")),
 					m("p", m("strong", "Eligible incomes: "), m("pre.program[id=income_eligibility]")),
 					m("p", m("strong", "Eligible gender: "), m("pre.program[id=gender_eligibility]")),
@@ -61,26 +49,38 @@ view: function(vnode) {
 					m("p", m("strong", "Required enrollment in: "), m("pre.program[id=other_program_enrollment]")),
 					m("p", m("strong", "Other eligibility: "), m("pre.program[id=other_eligibility]")),
 					m("p", m("strong", "Immigration status: "), m("pre.program[id=immigration_status]")),
+					m("p", m("strong", "Current accepted ID's"), m("pre.program[id=id_accepted_current]")),
+					m("p", m("strong", "Expired accepted ID's"), m("pre.program[id=id_accepted_expired]")),
+					m("p", m("strong", "Proof of address"), m("pre.program[id=proof_address]")),
+					m("p", m("strong", "Appointment required?: "), m("pre.program[id=appointment_required]")),
 					m("p", m("strong", "Services available on intake?"), m("pre.program[id=service_available_intake]")),
 					m("p", m("strong", "Intake notes: "), m("pre.program[id=service_available_intake_notes]")),
+					m("p", m("strong", "Full Physical Address: "), m("pre.program[id=physical_address]")),
+					m("p", m("strong", "Website: "), m("pre.program[id=website]")),
+					m("p", m("strong", "Website languages: "), m("pre.program[id=website_languages]")),
+					m("p", m("strong", "Service cost: "), m("pre.program[id=service_cost]")),
+					m("p", m("strong", "Payment options: "),m("pre.program[id=fee_structure]")),
 					m("p", m("strong", "Frontline staff languages: "), m("pre.program[id=frontline_languages]")),
+					m("p", m("strong", "Appointment notes: "), m("pre.program[id=appointment_required_notes]")),
 					m("p", m("strong", "Interpretation offered: "), m("pre.program[id=interpretation_offered]")),
+					m("p", m("strong", "Key words: "), m("pre.program[id=service_type]")),
 					m("p", m("strong", "Crisis-services offered: "), m("pre.program[id=crisis_services_offered]")),
+					m("p", m("strong", "Transportation: "), m("pre.program[id=transportation]")),
+					m("p", m("strong", "Schedule Notes: "), m("pre.program[id=schedule_notes]")),
+					m("p", m("strong", "Holiday Schedule: "), m("pre.program[id=holiday_schedule]")),
+					m("p", m("strong", "Required document links: "), m("pre.program[id=documents_required]")),
+					m("p", m("strong", "Document languages: "), m("pre.program[id=document_languages]")),
+					m("p", m("strong", "Form assistance offered: "), m("pre.program[id=document_assistance]")), 
 					m("p", m("strong", "Visual aid offered: "), m("pre.program[id=visual_aids_offered]")),
+					m("p", m("strong", "ADA acessible?"), m("pre.program[id=ada]")),
 					m("p", m("strong", "Client opportunity for paperwork consultation: "), m("pre.program[id=consultation_opportunity]")),
 					m("p", m("strong", "Has policy to respond to Immigration and Customs enforcement requests?: "), m("pre.program[id=enforcement_request_policy]")),
 					m("p", m("strong", "Cultural competency training offered: "), m("pre.program[id=cultural_competency_offered]")),
 
-
-					// //contact fields
-					m("p", m("strong", "Full Physical Address: "), m("pre.program[id=physical_address]")),
-					m("p", m("strong", "Website: "), m("pre.program[id=website]")),
-					m("p", m("strong", "ADA acessible?"), m("pre.program[id=ada]")),
-					m("p", m("strong", "Key words: "), m("pre.program[id=service_type]")),
-					m("p", m("strong", "Schedule Notes: "), m("pre.program[id=schedule_notes]")),
-					m("p", m("strong", "Holiday Schedule: "), m("pre.program[id=holiday_schedule]")),
-					m("p", m("strong", "Transportation: "), m("pre.program[id=transportation]")),
-					m("p", m("strong", "Website languages: "), m("pre.program[id=website_languages]"))
+					
+					
+					
+					
 			
 					
 				
