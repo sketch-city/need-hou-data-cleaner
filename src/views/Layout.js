f12e6ed538c54aa7d1fe4eeea673e62b4b50e752
Niha Pereira <niha.pereira@gmail.com>
2019-02-12 11:25:50 -0600

    added helpdesk link

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -10,7 +10,8 @@ module.exports = {
                 // m("div",
                 // m("div[id=myBar]")),
                 m("footer",
-                    m("p", "Click", m("a[href='mailto:gunjen@houstonimmgration.org']", " here "), " to learn more about the Immigrant Accessibility Profile.")
+                    m("p", "Click", m("a[href='mailto:gunjen@houstonimmgration.org']", " here "), " to learn more about the Immigrant Accessibility Profile."),
+                    m("p", "Have a feature request? Found a bug? Contact us", m("a[href=https://www.januaryadvisors.com/submit-feedback-or-report-a-bug][target=_blank]", " here "))
                 )
             )
         )
