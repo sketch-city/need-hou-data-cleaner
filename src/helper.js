3fffa0417fa9fe379a4e18a422e73d598fc9a0ae
Niha Pereira <niha.pereira@gmail.com>
2019-04-11 14:57:04 -0500

    added condition for lazier match on autocomplete

--- src/helper.js
+++ src/helper.js
@@ -93,7 +93,8 @@ function autocomplete(inp, arr, selectionKey = 'selected') {
       /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() ||
+              arr[i].substr(4, val.length).toUpperCase() == val.toUpperCase()) {
           /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
           /*make the matching letters bold:*/
